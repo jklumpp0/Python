a720ddf8f995d1116cc5a51d68457cfd5059af00
Milkjug <jklumpp0@gmail.com>
2016-08-04 12:03:27 -0700

    Add support for exif and creation time

--- crawl.py
+++ crawl.py
@@ -3,13 +3,27 @@ import os
 import sys
 from PIL import Image
 from db import DBImage, Database
+import datetime
+
+def ctime_to_datetime(ctime):
+    return datetime.datetime.strptime(ctime, "%a %b %d %H:%M:%S %Y")
+
+def get_exif_time(img):
+    try:
+        return img._getexif()[36867]
+    except:
+        return "1970:01:01 00:00:01"
 
 def get_image(file):
     try:
         img = Image.open(file)
         width, height = img.size
-        return DBImage(file, os.path.basename(file), width, height)
+        #print(img._getexif())
+        ctime = datetime.datetime.fromtimestamp(os.path.getctime(file))
+        exif_time = datetime.datetime.strptime(get_exif_time(img), "%Y:%m:%d %H:%M:%S")
+        return DBImage(file, os.path.basename(file), width, height, ctime, exif_time)
     except:
+        print(sys.exc_info())
         return None
 
 def crawl(folder = os.curdir): 
--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
@@ -12,15 +12,19 @@ class DBImage(Base):
     path = Column(String)
     width = Column(Integer)
     height = Column(Integer)
+    ctime = Column(DateTime)
+    exif_time = Column(DateTime)
 
-    def __init__(self, path, name, width, height):
+    def __init__(self, path, name, width, height, ctime, exif_time):
         self.path = path
         self.name = name
         self.width = width
         self.height = height
+        self.ctime = ctime
+        self.exif_time = exif_time
 
     def __repr__(self):
-        return "{}, width: {}, height: {}".format(self.name, self.width, self.height)
+        return "{}, width: {}, height: {}, ctime: {}, exif_time: {}".format(self.name, self.width, self.height, self.ctime, self.exif_time)
  
 class Database:
     def __init__(self):
